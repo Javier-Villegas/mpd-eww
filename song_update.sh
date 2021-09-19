5ee4db79fcf379492744b4e3f81f4b1abcb88e04
Javier-Villegas <javiervillegascarrasco@gmail.com>
2021-09-19 19:36:17 +0200

    Missed changes to song_update.sh

--- song_update.sh
+++ song_update.sh
@@ -1,1 +1,1 @@
-exec /home/javier/.config/eww/test.o "$(mpc -f %title% current)" "$(mpc -f %artist% current)" "$(mpc -f %file% current)"
+exec ./test.o "$(mpc -f %title% current)" "$(mpc -f %artist% current)" "$(mpc -f %file% current)"
