f657dd0707f89c9ddddf403e4c35bac45ea0b082
Javier-Villegas <javiervillegascarrasco@gmail.com>
2021-09-19 19:35:34 +0200

    Modified eww.yuck to be updated from outside

---
+++ song_update.sh
@@ -0,0 +1,1 @@
+exec /home/javier/.config/eww/test.o "$(mpc -f %title% current)" "$(mpc -f %artist% current)" "$(mpc -f %file% current)"
--- update-metadata.c
+++ update-metadata.c
@@ -6,21 +6,24 @@ void get_substring(int max_char, char * orig, char ** output);
 int main(int argc, char *argv[]){
 	// Check arguments
 	int max_char;
-	if (argc < 3){
-		printf("Needs 2 strings as input\n");
-	}else if (argc < 4){
+	if (argc < 4){
+		printf("Needs 3 strings as input\n");
+	}else if (argc < 5){
 		max_char = 20;
 	}else{
 		max_char = atoi(argv[1]);
 	}
+	printf()
 	char* input1;
 	char* input2;
 	get_substring(max_char, argv[1], &input1);
 	get_substring(max_char, argv[2], &input2);	
 	
-	char output[strlen(input1)+strlen(input2)+20];
-	sprintf(output,"command x %s %s\n", input1,input2);
+	char output[strlen(input1)+strlen(input2)+500];
+	sprintf(output,"ffmpeg -i /HDD/music/\"%s\" -y ~/.config/eww/test.png; ~/Documents/eww/target/release/eww update title=\"%s\" artist=\"%s\"\n", argv[3],input1,input2);
 	printf("%s", output);
+	printf("%s\n%s\n%s\n", argv[1], argv[2], argv[3]);
+	system(output);
 	return 0;
 
 }
@@ -41,16 +44,15 @@ void get_substring(int max_char, char * orig, char ** output){
 		i++;
 	}
 	//char output[i+4];
-	*output = (char *)malloc(i+5);
+	output[0] = (char *)malloc(i+5);
 	if (strlen(orig) < max_char){
-		strcpy(*output,orig);
+		strcpy(output[0],orig);
 	}else{
-		strncpy(*output,orig,i);
+		strncpy(output[0],orig,i);
 		output[0][i+3] = '\0';
 		output[0][i+2] = '.';		
 		output[0][i+1] = '.';
 		output[0][i] = '.';
 	}
-	printf("%s\n",*output);
 	return;
 }
