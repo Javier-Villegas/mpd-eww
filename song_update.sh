5f493fa085a5f29eeebe57d23e74c62488ad392a
Javier-Villegas <javiervillegascarrasco@gmail.com>
2021-09-20 19:49:09 +0200

    eww mpd-client image is manage dynamically, update-metada.c now reads song metadata and updates it while running

--- song_update.sh
+++
@@ -1,1 +0,0 @@
-exec ./test.o "$(mpc -f %title% current)" "$(mpc -f %artist% current)" "$(mpc -f %file% current)"
--- update-metadata.c
+++ update-metadata.c
@@ -1,36 +1,44 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-void get_substring(int max_char, char * orig, char ** output);
+#include<libmpd.h>
+#include <unistd.h>
+
+#define MAX_CHAR 20
+void get_substring(int max_char, char * orig);
+void status_changed(MpdObj *mi, ChangedStatusType what);
 
 int main(int argc, char *argv[]){
-	// Check arguments
-	int max_char;
-	if (argc < 4){
-		printf("Needs 3 strings as input\n");
-	}else if (argc < 5){
-		max_char = 20;
-	}else{
-		max_char = atoi(argv[1]);
-	}
-	printf()
-	char* input1;
-	char* input2;
-	get_substring(max_char, argv[1], &input1);
-	get_substring(max_char, argv[2], &input2);	
-	
-	char output[strlen(input1)+strlen(input2)+500];
-	sprintf(output,"ffmpeg -i /HDD/music/\"%s\" -y ~/.config/eww/test.png; ~/Documents/eww/target/release/eww update title=\"%s\" artist=\"%s\"\n", argv[3],input1,input2);
-	printf("%s", output);
-	printf("%s\n%s\n%s\n", argv[1], argv[2], argv[3]);
-	system(output);
-	return 0;
+	int run = 1, iport = 6600;
+    char *hostname = getenv("MPD_HOST");
+    char *port = getenv("MPD_PORT");
+    char *password = getenv("MPD_PASSWORD");
+    MpdObj *obj = NULL;
 
+	if(!hostname) {
+		hostname = "localhost";
+	}
+	if(port){
+		iport = atoi(port);
+	}
+
+	obj = mpd_new(hostname, iport,password);
+
+	mpd_signal_connect_status_changed(obj,(StatusChangedCallback)status_changed, NULL);
+	mpd_set_connection_timeout(obj, 10);
+
+	if(!mpd_connect(obj)){
+		mpd_send_password(obj);
+   		do{
+			mpd_status_update(obj);
+		}while(!usleep(100000) &&  run);
+	}
+	return 0;
 }
 
 
-void get_substring(int max_char, char * orig, char ** output){
-	//UTF16 is likely to exist in the input
+void get_substring(int max_char, char * orig){
+	//UTF8 encoding is likely to exist in the input
 	unsigned cnt=0, i;
 	for(i=0; cnt < max_char && i < strlen(orig);i++){
 		if((orig[i] & 0xF0) == 0xF0){
@@ -41,18 +49,34 @@ void get_substring(int max_char, char * orig, char ** output){
 			++i;
 		}
 		cnt++;
-		i++;
 	}
-	//char output[i+4];
-	output[0] = (char *)malloc(i+5);
-	if (strlen(orig) < max_char){
-		strcpy(output[0],orig);
-	}else{
-		strncpy(output[0],orig,i);
-		output[0][i+3] = '\0';
-		output[0][i+2] = '.';		
-		output[0][i+1] = '.';
-		output[0][i] = '.';
+	if (strlen(orig) > i){
+		orig[i-1] = '.';
+		orig[i-2] = '.';
+		orig[i-3] = '.';
 	}
+	orig[i] = '\0';
 	return;
+}
+
+void status_changed(MpdObj *mi, ChangedStatusType what){
+	if(what&MPD_CST_SONGID){
+		printf("Read song\n");
+		mpd_Song *song = mpd_playlist_get_current_song(mi);
+		printf("Done\n");
+		char output[500];
+		if (song == NULL ){
+			printf("Error getting metadata\n");
+			printf("Song change detected!\nTitle: Missing\nArtist: Missing\n");
+		}else{
+			if (song->title != NULL) get_substring(MAX_CHAR,song->title);
+			if (song->artist != NULL) get_substring(MAX_CHAR,song->artist);
+			
+			sprintf(output, "ffmpeg -i /HDD/music/\"%s\" -y ~/.config/eww/test.png; ~/Documents/eww/target/release/eww update title=\"%s\" artist=\"%s\" img=\"/home/javier/.config/eww/test.png\"",
+					song->file, song->title, song->artist);
+			system(output);
+			printf("Song change detected!\nTitle: %s\nArtist: %s\n",
+				song->title, song->artist);
+		}
+	}
 }
